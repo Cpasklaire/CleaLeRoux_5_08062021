2e6207f53704f889aa57f2e227adae408248f651
Cléa <80284454+Cpasklaire@users.noreply.github.com>
2021-09-01 16:02:55 +0200

    Update panier.js

--- public/js/panier.js
+++ public/js/panier.js
@@ -5,9 +5,15 @@ console.log(basketItems);
 for (let i = 0 ; i < basketItems.length; i++)
     {
         basketItems[i].idx = i;
-        const basketItem = basketItems[i]
+        const basketItem = basketItems[i];
         panierArticle(i);
-        fetchProduct(basketItems[i].id).then(data => panierDetails(basketItem, data));
+        fetchProduct(basketItems[i].id)
+            .then(data => 
+                {
+                    panierDetails(basketItem, data);
+                    panierTotal(data);
+                }
+            );
     }
 
         /*Création de la ligne par article*/
@@ -40,48 +46,14 @@ for (let i = 0 ; i < basketItems.length; i++)
 
             document.getElementById('details'+ basketItem.idx.toString()).appendChild(prix);
         }
-    /*Création de la ligne par article*/
-/*    function panierArticle(i)
-    {
-        article = document.createElement('div');
-        image = document.createElement('div');
-        contenu = document.createElement('div');
-        prix = document.createElement('div');
-        article.classList.add('row');
-        article.id = 'image'+ i.toString();
-        contenu.id = 'contenu'+ i.toString();
-        prix.id = 'prix'+ i.toString();
-        document.getElementById('article').appendChild(article);
-        document.getElementById('article').appendChild(contenu);
-        document.getElementById('article').appendChild(prix);
 
-    }
-
-    /*Création de l'article*/
-/*    function panierDetails(basketItem, data)
-    {
-        console.log(basketItem)
-        console.log(data)
-        /*image*/
-/*        image = document.createElement('div');
-        image.classList.add('col');
-        image.innerHTML += '<img class="img-thumbnail" alt="Ours en peluche' + data.name + '"src="'+data.imageUrl+'">';
-
-        document.getElementById('image'+ basketItem.idx.toString()).appendChild(image);
-
-        /*nom + couleur + quantité*/
-/*        contenu = document.createElement('div');
-        contenu.classList.add('col');
-        contenu.innerHTML += '<h2>'+data.name+'</h2><span>'+basketItem.color+'</spam><spam>'+basketItem.qty+'</span>';
-
-        document.getElementById('contenu'+ basketItem.idx.toString()).appendChild(contenu);
-
-        /*prix*/
-//        prix = document.createElement('div');
-//        prix.classList.add('col');
-//        prix.innerHTML += '<div class="col">'+data.price+'</div>'
-
-//        console.log('prix' + basketItem.idx.toString())
-//        console.log(document.getElementById('prix' + basketItem.idx.toString()))
-//        document.getElementById('prix' + basketItem.idx.toString()).appendChild(prix);
-//    }
+        function panierTotal(data)
+        {
+            let total = 0;
+            for (let i = 0 ; i < basketItems.length; i++)
+            {
+                const array = basketItems.length
+            total += Number(data.price);
+            console.log(reducer);
+            }
+        }
